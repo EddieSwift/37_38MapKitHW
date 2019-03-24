74697cba9c076c4bd14bade526b41532f06d4fac
Eduard Galchenko <eddiegalchenko@gmail.com>
2019-03-24 10:36:08 +0200

    Initial Commit

---
+++ 37_38MapKitHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  37_38MapKitHW
+//
+//  Created by Eduard Galchenko on 3/24/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 37_38MapKitHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  37_38MapKitHW
+//
+//  Created by Eduard Galchenko on 3/24/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
